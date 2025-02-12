2858978a7e6a82643cbbf83e758fccd6b7b41197
Abdul Malik <167233705+NizAdal@users.noreply.github.com>
2025-02-12 18:44:27 +0500

    Update Work.jsx

--- src/component/screens/Work.jsx
+++ src/component/screens/Work.jsx
@@ -8,7 +8,7 @@ function Work() {
          <motion.div 
       initial={{ opacity: 0, y: 50}}
       whileInView={{ opacity:1, y: 0}}
-      viewport={{ once: false, amount: 0.5 }}
+      viewport={{ once: false, amount: 0.2 }}
       transition={{ duration: 1}} 
       >    
             <div className='pb-8'>
