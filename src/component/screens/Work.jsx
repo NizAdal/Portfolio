cf427478ed4b2915bb7c4eae53efbe7a6dd816d0
Abdul Malik <167233705+NizAdal@users.noreply.github.com>
2026-02-15 00:50:54 +0500

    Replace project images and update associated links
    Updated images and links for project showcases in Work.jsx.

--- src/component/screens/Work.jsx
+++ src/component/screens/Work.jsx
@@ -35,7 +35,7 @@ function Work() {
                 <div className='transform transition-transform duration-300 hover:scale-105 overflow-hidden
                 shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center
                 mx-auto content-div h-[200px] bg-cover relative'>
-                    <img src="./Nizam.JPG" alt="NEW image" layout="fill" objectFit="cover" />
+                    <img src="./logo.png" alt="NEW image" layout="fill" objectFit="cover" />
                 <div className='opacity-0 group-hover:opacity-90 bg-[gray]/70
                 absolute inset-0 flex flex-col justify-center items-center'>
                     <span className='text-2xl font-bold text-white tracking-wider ml-7'>Nizam Eduction Welfare(NEW) <br/>In Django</span>
@@ -47,10 +47,27 @@ function Work() {
                     </a>
                 </div>    
                 </div>
+
                 <div className='transform transition-transform duration-300 hover:scale-105 overflow-hidden
                 shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center
                 mx-auto content-div h-[200px] bg-cover relative'>
-                    <img src="./Nizam.JPG" alt="NEW image" className="w-full h-full object-cover" />
+                    <img src="./customer behavior.png" alt="Customer Behavior Dashboard" className="w-full h-full object-cover" />
+                <div className='opacity-0 group-hover:opacity-90 bg-[gray]/70
+                absolute inset-0 flex flex-col justify-center items-center'>
+                    <span className='text-2xl font-bold text-white tracking-wider ml-7'>Nizam Eduction Welfare(NEW) <br/> In React </span>
+                    <a href="https://github.com/NizAdal/customer_shopping_behavior_analysis" target='_blank'>
+                        <button className='text-center rounded-lg px-4 py-3 m-2
+                         text-gray-700 font-bold text-lg bg-white cursor-pointer'>
+                            Check Code
+                        </button>
+                    </a>
+                </div>    
+                </div>
+              
+                <div className='transform transition-transform duration-300 hover:scale-105 overflow-hidden
+                shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center
+                mx-auto content-div h-[200px] bg-cover relative'>
+                    <img src="./Dashboard" alt="Merchant Sales Dashboard" className="w-full h-full object-cover" />
                 <div className='opacity-0 group-hover:opacity-90 bg-[gray]/70
                 absolute inset-0 flex flex-col justify-center items-center'>
                     <span className='text-2xl font-bold text-white tracking-wider ml-7'>Nizam Eduction Welfare(NEW) <br/> In React </span>
